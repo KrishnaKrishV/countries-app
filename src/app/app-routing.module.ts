43caa5283351b889b347f87b039c03d21b35c6db
thulasidass04 <thulasidass04@gmail.com>
2019-12-25 08:52:53 +0530

    countries completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CountriesComponent } from './countries/countries.component';
-import { CountriesListComponent } from './countries-list/countries-list.component';
-
 
 const routes: Routes = [
   {
     path: 'countries',
-    component: CountriesComponent
-  },
-  {
-    path: 'countries/:name',
-    component: CountriesListComponent
+    loadChildren: () => import('./countries/countries.module').then(m => m.CountriesModule),
   },
   {
     path: 'employees',
     loadChildren: () => import('./employee/employee.module').then(m => m.EmployeeModule),
+  },
+  {
+    path: 'places',
+    loadChildren: () => import('./places/places.module').then(m => m.PlacesModule), 
   },
   {
     path: '',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { CountriesComponent } from './countries/countries.component';
-import { CountryNameComponent } from './country-name/country-name.component';
-import { CountriesListComponent } from './countries-list/countries-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CountriesComponent,
-    CountryNameComponent,
-    CountriesListComponent,
   ],
   imports: [
     BrowserModule,
